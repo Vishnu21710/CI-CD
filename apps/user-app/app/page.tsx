836d073ef5b95136433249e5b14535e394b28f94
Vishnu21710 <vishnu.mupparathy21@gmail.com>
2024-06-15 10:42:16 +0530

    test-4

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -12,6 +12,7 @@ export default async function Page() {
 
   const session = await getServerSession(authOptions);
   if (session?.user) {
+    //testing CICD
     redirect('/dashboard')
   } else {
     redirect('/api/auth/signin')
