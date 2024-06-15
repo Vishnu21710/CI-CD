a9de380b3da0f07b0bc301bead8c14659f3593aa
Vishnu21710 <vishnu.mupparathy21@gmail.com>
2024-06-15 10:39:01 +0530

    testing - 3

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -13,7 +13,7 @@ export default async function Page() {
   const session = await getServerSession(authOptions);
   if (session?.user) {
 
-    
+    //test
     redirect('/dashboard')
   } else {
     redirect('/api/auth/signin')
